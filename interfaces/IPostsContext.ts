bd2f35a13877a527562a453abc51f1ac1882f1c1
iva.zlopasa <iva.zlopasa9@gmail.com>
2022-01-17 10:56:01 +0100

    filter+images

--- interfaces/IPostsContext.ts
+++ interfaces/IPostsContext.ts
@@ -1,7 +1,9 @@
 import { TAuthor } from "./TAuthors";
+import { TImage } from "./TImage";
 import { TPost } from "./TPost";
 
 export default interface IPostsContext {
     posts: TPost[] | undefined;
     authors: TAuthor[] | undefined;
+    images: TImage [] | undefined
   }
---
+++ interfaces/TImage.ts
@@ -0,0 +1,7 @@
+export type TImage = {
+    albumId: number;
+    id: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
+};
